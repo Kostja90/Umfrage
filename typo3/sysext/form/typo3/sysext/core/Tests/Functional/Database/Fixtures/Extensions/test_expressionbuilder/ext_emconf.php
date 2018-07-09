f26d1d9bc14fce4746ff7cc6bf0dae02319a37d6
Kostja90 <Jahuddinator1>
2018-07-09 15:07:53 +0200

    git ignore und compressor dateien hinzugefügt die größe ist aber unbekannt

---
+++ typo3/sysext/form/typo3/sysext/core/Tests/Functional/Database/Fixtures/Extensions/test_expressionbuilder/ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'ExpressionBuilder Test',
+    'description' => 'ExpressionBuilder Test',
+    'category' => 'example',
+    'version' => '8.7.15',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Christian Kuhn',
+    'author_email' => 'lolli@schwarzbu.ch',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.15',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
