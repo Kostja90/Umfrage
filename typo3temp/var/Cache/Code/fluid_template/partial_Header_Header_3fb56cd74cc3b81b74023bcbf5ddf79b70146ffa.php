0498df95bce4dfc15bbc8bd2ae9c9e88ae5bac26
Kostja90 <Jahuddinator1>
2018-07-09 13:56:57 +0200

    compresor hinzugefügt und seite so bearbeitet, dass auf der Hauptseite die Umfrage auch angezeigt wird

---
+++ typo3temp/var/Cache/Code/fluid_template/partial_Header_Header_3fb56cd74cc3b81b74023bcbf5ddf79b70146ffa.php
@@ -0,0 +1,7 @@
+<?php
+
+class partial_Header_Header_3fb56cd74cc3b81b74023bcbf5ddf79b70146ffa extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate
+ implements \TYPO3Fluid\Fluid\Core\Compiler\UncompilableTemplateInterface
+{
+}
+#
---
+++ typo3temp/var/Cache/Code/fluid_template/partial_Header_SubHeader_7b4256fb2850c6daf659685002c0ac21ccaf2b22.php
@@ -0,0 +1,7 @@
+<?php
+
+class partial_Header_SubHeader_7b4256fb2850c6daf659685002c0ac21ccaf2b22 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate
+ implements \TYPO3Fluid\Fluid\Core\Compiler\UncompilableTemplateInterface
+{
+}
+#
