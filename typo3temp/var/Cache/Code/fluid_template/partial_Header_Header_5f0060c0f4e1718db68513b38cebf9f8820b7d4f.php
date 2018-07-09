df03bee496bd40cf1cd04f7ddcba5496de254f62
Kostja90 <Jahuddinator1>
2018-07-09 10:22:21 +0200

    not sure what i did

---
+++ typo3temp/var/Cache/Code/fluid_template/partial_Header_Header_5f0060c0f4e1718db68513b38cebf9f8820b7d4f.php
@@ -0,0 +1,7 @@
+<?php
+
+class partial_Header_Header_5f0060c0f4e1718db68513b38cebf9f8820b7d4f extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate
+ implements \TYPO3Fluid\Fluid\Core\Compiler\UncompilableTemplateInterface
+{
+}
+#
---
+++ typo3temp/var/Cache/Code/fluid_template/partial_Header_SubHeader_6562b34694a6b220a9af7124813ce8ee2e5f1d0c.php
@@ -0,0 +1,7 @@
+<?php
+
+class partial_Header_SubHeader_6562b34694a6b220a9af7124813ce8ee2e5f1d0c extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate
+ implements \TYPO3Fluid\Fluid\Core\Compiler\UncompilableTemplateInterface
+{
+}
+#
