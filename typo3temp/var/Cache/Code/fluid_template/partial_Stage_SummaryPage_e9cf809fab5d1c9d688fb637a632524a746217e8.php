10095fdf51620dd2cda4e82e42b9bce858a7f008
Kostja90 <Jahuddinator1>
2018-07-09 10:06:00 +0200

    alte Umfrage hinzugefügt
    alt Umfrage hinzugefügt undden Rcode für YAML Datei

---
+++ typo3temp/var/Cache/Code/fluid_template/partial_Stage_SummaryPage_e9cf809fab5d1c9d688fb637a632524a746217e8.php
@@ -0,0 +1,44 @@
+<?php
+
+class partial_Stage_SummaryPage_e9cf809fab5d1c9d688fb637a632524a746217e8 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this; 
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+
+return '
+<h2 data-template-property="label"></h2>
+
+';
+}
+
+
+}
+#
